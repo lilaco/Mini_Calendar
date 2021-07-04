bbba6b0e4054491011493b42c00f79fc54476c25
gray0108 <jiung0108@gmail.com>
2021-07-05 00:00:01 +0900

    Added event modal function

--- 04-Tutorial/script.js
+++ 04-Tutorial/script.js
@@ -5,13 +5,30 @@ let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
+const newEventModal = document.getElementById('newEventModal');
+const backDrop = document.getElementById('modalBackDrop');
 const weekdays = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+
+function openModal(date) {
+    clicked = date;
+
+    // find function
+    const eventForDay = events.find(e => e.date === clicked);
+
+    if (eventForDay) {
+        console.log('이벤트가 이미 존재합니다.');
+    } else {
+        newEventModal.style.display = 'block';
+    }
+
+    backDrop.style.display = 'block';
+}
 
 function load() {
     const dt = new Date();
 
     //This related on next:back button
-    if(nav != 0) {
+    if(nav !== 0) {
         dt.setMonth(new Date().getMonth() + nav);
     }
 
@@ -33,8 +50,9 @@ function load() {
     const paddingDays = weekdays.indexOf(dateString.split('. ')[3]);
 
     // 월, 연도 표시.
+    // document.getElementById('monthDisplay').innerText(dt.toLocaleDateString('ko-KR',{ month: 'long' }) + " " + year);
     document.getElementById('monthDisplay').innerText = 
-        `${dt.toLocaleDateString('ko-KR', { month: 'long'})} ${year}`;
+    `${dt.toLocaleDateString('ko-KR', { month: 'long' })} ${year}`;
 
     calendar.innerHTML = '';
 
@@ -46,7 +64,8 @@ function load() {
         if(i>paddingDays) {
             daySquare.innerText = i - paddingDays;
 
-            daySquare.addEventListener('click', () => console.log('click'));
+            // daySquare.addEventListener('click', () => console.log('click'));
+            daySquare.addEventListener('click', () => openModal());
         } else {
             daySquare.classList.add('padding');
         }
